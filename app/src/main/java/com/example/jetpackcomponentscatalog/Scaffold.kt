b6153f17bc3d4ca930b68032aaa91043f1fa716c
TomasMarquez <tomasmarquez@erfri.com>
2023-12-11 14:56:50 +0100

    T11-12-23

--- app/src/main/java/com/example/jetpackcomponentscatalog/Scaffold.kt
+++ app/src/main/java/com/example/jetpackcomponentscatalog/Scaffold.kt
@@ -1,32 +1,48 @@
 package com.example.jetpackcomponentscatalog
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.IconButton
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ScaffoldExample() {
-    val scaffolState = rememberScaffoldState()
-    Scaffold(topBar = { MyTopAppBar() }) {
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = { MyTopAppBar { coroutineScope.launch { snackbarHostState.showSnackbar("Has pulsado $it") } } },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                snackbar = {
+                    Snackbar(
+                        snackbarData = it,
+                        contentColor = Color.Blue
+                    )
+                })
+        }) {
         /*Box(modifier = Modifier
             .height(50.dp)
             .fillMaxWidth()
@@ -38,23 +54,23 @@ fun ScaffoldExample() {
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Mi primera toolbar") },
         backgroundColor = Color.Gray,
         contentColor = Color.Black,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Atras") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
         },
         actions = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "search")
             }
-            IconButton(onClick = {}) {
-                Icon(imageVector = Icons.Filled.FavoriteBorder, contentDescription = "favorite")
+            IconButton(onClick = { onClickIcon("Familia") }) {
+                Icon(imageVector = Icons.Filled.Favorite, contentDescription = "family")
             }
         }
     )
